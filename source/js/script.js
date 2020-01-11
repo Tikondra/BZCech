d907cc6021d00b47409b539cd6260ad5c14ac10a
Tikondra <tikondra47@gmail.com>
2020-01-11 14:59:42 +0400

    test

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 function menu() {
-  let menuToggle = document.querySelector('.navigation__btn');
-  let menu = document.querySelector('.navigation__list');
+  var menuToggle = document.querySelector('.navigation__btn');
+  var menu = document.querySelector('.navigation__list');
 
   menuToggle.addEventListener('click', function () {
     menu.classList.toggle('navigation__list--active');
