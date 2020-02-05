63e3d080722fe1523e2e639d36742ff0c0e5fb3a
Tikondra <tikondra47@gmail.com>
2020-02-05 14:41:18 +0400

    formaD

--- source/js/script.js
+++ source/js/script.js
@@ -10,6 +10,6 @@ function menu() {
 menu();
 
 $(document).ready(function(){
-  $('.callback__input').inputmask("+7 (999) 999-99-99");
+  $('.form__input--js').inputmask("+7 (999) 999-99-99");
 });
 
